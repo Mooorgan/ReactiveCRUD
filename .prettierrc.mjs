6133708d714bd0df9f9b164a0fee97fdbce35ee9
Shaishav Paudel <morganhouse15@gmail.com>
2024-08-15 11:54:18 +0545

    chore:Add prettier package

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+};
+
+export default config;
